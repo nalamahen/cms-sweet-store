4b7a4d4f47c438a8ccab12fe0b52d1b359430b0d
Nala Mahen <mail2mahen@yahoo.co.uk>
2018-09-20 01:57:22 +0100

    Add env varibales

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,7 @@
-/*
-if(process.env.MODE_ENV=== 'production') {
+if(process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
     module.exports = require('./prod');
-}else {
+} else {
+    // we are in development - return the dev keys
     module.exports = require('./dev');
 }
-*/
-
-module.exports = require('./prod');
