3b55e62d68176a847318ccd0b36e1aec95e92bfe
Nala Mahen <mail2mahen@yahoo.co.uk>
2018-10-29 12:46:56 +0000

    Resolve incorrect path issue site js file.

---
+++ public/assets/js/site.js
@@ -0,0 +1,18 @@
+$(function() {
+    if ($('textarea#ta').length) {
+         CKEDITOR.replace('ta');
+     }
+ 
+     $('a.confirmDeletion').on('click', function () {
+         if (!confirm('Confirm deletion'))
+             return false;
+     });
+     
+     if ($("[data-fancybox]").length) {
+         $("[data-fancybox]").fancybox();
+     }    
+ 
+     $(".alert").fadeTo(2000, 500).slideUp(500, function () {
+         $(".alert").alert('close');
+     });
+ });
